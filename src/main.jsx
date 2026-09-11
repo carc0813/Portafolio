82e18ec8c43dff54830fe88d5b897e46c619a976
carc0813 <carc1023@hotmail.com>
2026-09-11 17:02:27 -0500

    Corregir ruta de estilos para GitHub Actions

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-import "./styles/global.css";
+import "./Styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
